e5dfb977a908ff05ebe9024a0bfb2244dad36845
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2018-12-29 07:48:10 -0800

    fix git changes my files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,163 +32,163 @@
          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
  <script>
-         ! function(i) {
-             function e(e) {
-                 for (var t, r, n = e[0], o = e[1], a = e[2], u = 0, c = []; u < n.length; u++) r = n[u], s[r] && c.push(s[r][0]), s[r] = 0;
-                 for (t in o) Object.prototype.hasOwnProperty.call(o, t) && (i[t] = o[t]);
-                 for (p && p(e); c.length;) c.shift()();
-                 return l.push.apply(l, a || []), f()
-             }
+        ! function(i) {
+            function e(e) {
+                for (var t, r, n = e[0], o = e[1], a = e[2], u = 0, c = []; u < n.length; u++) r = n[u], s[r] && c.push(s[r][0]), s[r] = 0;
+                for (t in o) Object.prototype.hasOwnProperty.call(o, t) && (i[t] = o[t]);
+                for (p && p(e); c.length;) c.shift()();
+                return l.push.apply(l, a || []), f()
+            }
 
-             function f() {
-                 for (var e, t = 0; t < l.length; t++) {
-                     for (var r = l[t], n = !0, o = 1; o < r.length; o++) {
-                         var a = r[o];
-                         0 !== s[a] && (n = !1)
-                     }
-                     n && (l.splice(t--, 1), e = d(d.s = r[0]))
-                 }
-                 return e
-             }
-             var r = {},
-                 c = {
-                     11: 0
-                 },
-                 s = {
-                     11: 0
-                 },
-                 l = [];
+            function f() {
+                for (var e, t = 0; t < l.length; t++) {
+                    for (var r = l[t], n = !0, o = 1; o < r.length; o++) {
+                        var a = r[o];
+                        0 !== s[a] && (n = !1)
+                    }
+                    n && (l.splice(t--, 1), e = d(d.s = r[0]))
+                }
+                return e
+            }
+            var r = {},
+                c = {
+                    11: 0
+                },
+                s = {
+                    11: 0
+                },
+                l = [];
 
-             function d(e) {
-                 if (r[e]) return r[e].exports;
-                 var t = r[e] = {
-                     i: e,
-                     l: !1,
-                     exports: {}
-                 };
-                 return i[e].call(t.exports, t, t.exports, d), t.l = !0, t.exports
-             }
-             d.e = function(l) {
-                 var e = [];
-                 c[l] ? e.push(c[l]) : 0 !== c[l] && {
-                     4: 1,
-                     5: 1
-                 }[l] && e.push(c[l] = new Promise(function(e, n) {
-                     for (var t = "static/css/" + ({}[l] || l) + "." + {
-                             0: "31d6cfe0",
-                             2: "31d6cfe0",
-                             3: "31d6cfe0",
-                             4: "38898ce5",
-                             5: "4fa85488",
-                             6: "31d6cfe0",
-                             7: "31d6cfe0",
-                             8: "31d6cfe0",
-                             10: "31d6cfe0"
-                         }[l] + ".chunk.css", o = d.p + t, r = document.getElementsByTagName("link"), a = 0; a < r.length; a++) {
-                         var u = (i = r[a]).getAttribute("data-href") || i.getAttribute("href");
-                         if ("stylesheet" === i.rel && (u === t || u === o)) return e()
-                     }
-                     var c = document.getElementsByTagName("style");
-                     for (a = 0; a < c.length; a++) {
-                         var i;
-                         if ((u = (i = c[a]).getAttribute("data-href")) === t || u === o) return e()
-                     }
-                     var f = document.createElement("link");
-                     f.rel = "stylesheet", f.type = "text/css", f.onload = e, f.onerror = function(e) {
-                         var t = e && e.target && e.target.src || o,
-                             r = new Error("Loading CSS chunk " + l + " failed.\n(" + t + ")");
-                         r.request = t, n(r)
-                     }, f.href = o, document.getElementsByTagName("head")[0].appendChild(f)
-                 }).then(function() {
-                     c[l] = 0
-                 }));
-                 var r = s[l];
-                 if (0 !== r)
-                     if (r) e.push(r[2]);
-                     else {
-                         var t = new Promise(function(e, t) {
-                             r = s[l] = [e, t]
-                         });
-                         e.push(r[2] = t);
-                         var n, o = document.getElementsByTagName("head")[0],
-                             a = document.createElement("script");
-                         a.charset = "utf-8", a.timeout = 120, d.nc && a.setAttribute("nonce", d.nc), a.src = d.p + "static/js/" + ({}[l] || l) + "." + {
-                             0: "6a37f1b6",
-                             2: "fca3f2d3",
-                             3: "942f4533",
-                             4: "4053d475",
-                             5: "abe25c2a",
-                             6: "32a7b70d",
-                             7: "f15b0e1e",
-                             8: "1d83392c",
-                             10: "e8ce7720"
-                         }[l] + ".chunk.js", n = function(e) {
-                             a.onerror = a.onload = null, clearTimeout(u);
-                             var t = s[l];
-                             if (0 !== t) {
-                                 if (t) {
-                                     var r = e && ("load" === e.type ? "missing" : e.type),
-                                         n = e && e.target && e.target.src,
-                                         o = new Error("Loading chunk " + l + " failed.\n(" + r + ": " + n + ")");
-                                     o.type = r, o.request = n, t[1](o)
-                                 }
-                                 s[l] = void 0
-                             }
-                         };
-                         var u = setTimeout(function() {
-                             n({
-                                 type: "timeout",
-                                 target: a
-                             })
-                         }, 12e4);
-                         a.onerror = a.onload = n, o.appendChild(a)
-                     }
-                 return Promise.all(e)
-             }, d.m = i, d.c = r, d.d = function(e, t, r) {
-                 d.o(e, t) || Object.defineProperty(e, t, {
-                     enumerable: !0,
-                     get: r
-                 })
-             }, d.r = function(e) {
-                 "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
-                     value: "Module"
-                 }), Object.defineProperty(e, "__esModule", {
-                     value: !0
-                 })
-             }, d.t = function(t, e) {
-                 if (1 & e && (t = d(t)), 8 & e) return t;
-                 if (4 & e && "object" == typeof t && t && t.__esModule) return t;
-                 var r = Object.create(null);
-                 if (d.r(r), Object.defineProperty(r, "default", {
-                         enumerable: !0,
-                         value: t
-                     }), 2 & e && "string" != typeof t)
-                     for (var n in t) d.d(r, n, function(e) {
-                         return t[e]
-                     }.bind(null, n));
-                 return r
-             }, d.n = function(e) {
-                 var t = e && e.__esModule ? function() {
-                     return e.default
-                 } : function() {
-                     return e
-                 };
-                 return d.d(t, "a", t), t
-             }, d.o = function(e, t) {
-                 return Object.prototype.hasOwnProperty.call(e, t)
-             }, d.p = "/", d.oe = function(e) {
-                 throw console.error(e), e
-             };
-             var t = window.webpackJsonp = window.webpackJsonp || [],
-                 n = t.push.bind(t);
-             t.push = e, t = t.slice();
-             for (var o = 0; o < t.length; o++) e(t[o]);
-             var p = n;
-             f()
-         }([])
-     </script>
+            function d(e) {
+                if (r[e]) return r[e].exports;
+                var t = r[e] = {
+                    i: e,
+                    l: !1,
+                    exports: {}
+                };
+                return i[e].call(t.exports, t, t.exports, d), t.l = !0, t.exports
+            }
+            d.e = function(l) {
+                var e = [];
+                c[l] ? e.push(c[l]) : 0 !== c[l] && {
+                    4: 1,
+                    5: 1
+                }[l] && e.push(c[l] = new Promise(function(e, n) {
+                    for (var t = "static/css/" + ({}[l] || l) + "." + {
+                            0: "31d6cfe0",
+                            2: "31d6cfe0",
+                            3: "31d6cfe0",
+                            4: "38898ce5",
+                            5: "4fa85488",
+                            6: "31d6cfe0",
+                            7: "31d6cfe0",
+                            8: "31d6cfe0",
+                            10: "31d6cfe0"
+                        }[l] + ".chunk.css", o = d.p + t, r = document.getElementsByTagName("link"), a = 0; a < r.length; a++) {
+                        var u = (i = r[a]).getAttribute("data-href") || i.getAttribute("href");
+                        if ("stylesheet" === i.rel && (u === t || u === o)) return e()
+                    }
+                    var c = document.getElementsByTagName("style");
+                    for (a = 0; a < c.length; a++) {
+                        var i;
+                        if ((u = (i = c[a]).getAttribute("data-href")) === t || u === o) return e()
+                    }
+                    var f = document.createElement("link");
+                    f.rel = "stylesheet", f.type = "text/css", f.onload = e, f.onerror = function(e) {
+                        var t = e && e.target && e.target.src || o,
+                            r = new Error("Loading CSS chunk " + l + " failed.\n(" + t + ")");
+                        r.request = t, n(r)
+                    }, f.href = o, document.getElementsByTagName("head")[0].appendChild(f)
+                }).then(function() {
+                    c[l] = 0
+                }));
+                var r = s[l];
+                if (0 !== r)
+                    if (r) e.push(r[2]);
+                    else {
+                        var t = new Promise(function(e, t) {
+                            r = s[l] = [e, t]
+                        });
+                        e.push(r[2] = t);
+                        var n, o = document.getElementsByTagName("head")[0],
+                            a = document.createElement("script");
+                        a.charset = "utf-8", a.timeout = 120, d.nc && a.setAttribute("nonce", d.nc), a.src = d.p + "static/js/" + ({}[l] || l) + "." + {
+                            0: "6a37f1b6",
+                            2: "fca3f2d3",
+                            3: "942f4533",
+                            4: "4053d475",
+                            5: "abe25c2a",
+                            6: "32a7b70d",
+                            7: "f15b0e1e",
+                            8: "1d83392c",
+                            10: "e8ce7720"
+                        }[l] + ".chunk.js", n = function(e) {
+                            a.onerror = a.onload = null, clearTimeout(u);
+                            var t = s[l];
+                            if (0 !== t) {
+                                if (t) {
+                                    var r = e && ("load" === e.type ? "missing" : e.type),
+                                        n = e && e.target && e.target.src,
+                                        o = new Error("Loading chunk " + l + " failed.\n(" + r + ": " + n + ")");
+                                    o.type = r, o.request = n, t[1](o)
+                                }
+                                s[l] = void 0
+                            }
+                        };
+                        var u = setTimeout(function() {
+                            n({
+                                type: "timeout",
+                                target: a
+                            })
+                        }, 12e4);
+                        a.onerror = a.onload = n, o.appendChild(a)
+                    }
+                return Promise.all(e)
+            }, d.m = i, d.c = r, d.d = function(e, t, r) {
+                d.o(e, t) || Object.defineProperty(e, t, {
+                    enumerable: !0,
+                    get: r
+                })
+            }, d.r = function(e) {
+                "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+                    value: "Module"
+                }), Object.defineProperty(e, "__esModule", {
+                    value: !0
+                })
+            }, d.t = function(t, e) {
+                if (1 & e && (t = d(t)), 8 & e) return t;
+                if (4 & e && "object" == typeof t && t && t.__esModule) return t;
+                var r = Object.create(null);
+                if (d.r(r), Object.defineProperty(r, "default", {
+                        enumerable: !0,
+                        value: t
+                    }), 2 & e && "string" != typeof t)
+                    for (var n in t) d.d(r, n, function(e) {
+                        return t[e]
+                    }.bind(null, n));
+                return r
+            }, d.n = function(e) {
+                var t = e && e.__esModule ? function() {
+                    return e.default
+                } : function() {
+                    return e
+                };
+                return d.d(t, "a", t), t
+            }, d.o = function(e, t) {
+                return Object.prototype.hasOwnProperty.call(e, t)
+            }, d.p = "/", d.oe = function(e) {
+                throw console.error(e), e
+            };
+            var t = window.webpackJsonp = window.webpackJsonp || [],
+                n = t.push.bind(t);
+            t.push = e, t = t.slice();
+            for (var o = 0; o < t.length; o++) e(t[o]);
+            var p = n;
+            f()
+        }([])
+    </script>
     <script src="{{ asset('static/js/9.9a7ae88e.chunk.js')}}"></script>
-    <script src="{{ asset('static/js/main.49411cff.chunk.js')}}"></script>
+    <script src="{{ asset('static/js/main.5ba61bd8.chunk.js')}}"></script>
      <script>
      $(document).ready(function () {
 
