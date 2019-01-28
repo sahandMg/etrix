e6706e4f4ae3cbca312e7ef23bb5b74daca83e13
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2019-01-28 04:55:21 -0800

    test new search with

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,78 +1,45 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="keywords" content="مقالات آموزشی الکترونیک,آی سی,ای سی,ماژول,مقاومت,خازن,ترانزیستور,فروش اینترنتی,الکترونیک,آردوینو,رباتیک,میکروکنترلر,قطعات الکترونیک,فروش">
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <meta name="description"
-                  content="فروشگاه آنلاین قطعات الکترونیک و مجله آموزشی الکترونیک"/>
 
-        <meta name="google-site-verification" content="4DHhbhIWzWzQradPyRcPiW-5nlgcWdcQWX4fosAHVkI" />
-
-=======
-        <meta name="description"
-            content="فروشگاه آنلاین قطعات الکترونیک و مجله آموزشی الکترونیک"/>
-        <meta name="google-site-verification" content="4DHhbhIWzWzQradPyRcPiW-5nlgcWdcQWX4fosAHVkI" />
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-=======
-        <meta name="description"
-            content="فروشگاه آنلاین قطعات الکترونیک و مجله آموزشی الکترونیک"/>
-        <meta name="google-site-verification" content="4DHhbhIWzWzQradPyRcPiW-5nlgcWdcQWX4fosAHVkI" />
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-        <title>ETRIX - فروشگاه قطعات الکترونیک</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="مقالات آموزشی الکترونیک,آی سی,ای سی,ماژول,مقاومت,خازن,ترانزیستور,فروش اینترنتی,الکترونیک,آردوینو,رباتیک,میکروکنترلر,قطعات الکترونیک,فروش">
+    <meta name="description" content="فروشگاه آنلاین قطعات الکترونیک و مجله آموزشی الکترونیک" />
+    <meta name="google-site-verification" content="4DHhbhIWzWzQradPyRcPiW-5nlgcWdcQWX4fosAHVkI" />
+    <title>ETRIX-فروشگاه قطعات الکترونیک</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="{{asset('static/css/11.c431ba77.chunk.css')}}" rel="stylesheet">
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <link href="{{asset('static/css/main.2d392d3f.chunk.css')}}" rel="stylesheet">
-=======
-    <link href="{{asset('static/css/main.1e2b74d5.chunk.css')}}" rel="stylesheet">
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-=======
-    <link href="{{asset('static/css/main.1e2b74d5.chunk.css')}}" rel="stylesheet">
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-    </head>
-    <body>
-     <div id="example">
-     </div>
+    <link href="{{asset('static/css/11.afa69c0a.chunk.css')}}" rel="stylesheet">
+    <link href="{{asset('static/css/main.88c0f9e0.chunk.css')}}" rel="stylesheet">
+</head>
 
-     <style>
-     @font-face {
-         font-family: myFirstFont;
-         src: url(font/3.woff);
-     }
-
-     body {
-         font-family: myFirstFont;
-     }
-     </style>
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+<body>
+    <div id="example"></div>
+    <style>
+        @font-face {
+            font-family: myFirstFont;
+            src: url(font/3.woff)
+        }
+        
+        body {
+            font-family: myFirstFont
+        }
+    </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
     <script>
-        ! function(f) {
+        ! function(i) {
             function e(e) {
-<<<<<<< HEAD
-<<<<<<< HEAD
                 for (var t, r, n = e[0], o = e[1], a = e[2], u = 0, c = []; u < n.length; u++) r = n[u], s[r] && c.push(s[r][0]), s[r] = 0;
-                for (t in o) Object.prototype.hasOwnProperty.call(o, t) && (f[t] = o[t]);
-                for (p && p(e); c.length;) c.shift()();
-                return l.push.apply(l, a || []), i()
-=======
-=======
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-                for (var t, r, n = e[0], o = e[1], a = e[2], c = 0, u = []; c < n.length; c++) r = n[c], s[r] && u.push(s[r][0]), s[r] = 0;
                 for (t in o) Object.prototype.hasOwnProperty.call(o, t) && (i[t] = o[t]);
-                for (p && p(e); u.length;) u.shift()();
+                for (p && p(e); c.length;) c.shift()();
                 return l.push.apply(l, a || []), f()
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
             }
 
-            function i() {
+            function f() {
                 for (var e, t = 0; t < l.length; t++) {
                     for (var r = l[t], n = !0, o = 1; o < r.length; o++) {
                         var a = r[o];
@@ -83,15 +50,7 @@
                 return e
             }
             var r = {},
-<<<<<<< HEAD
-<<<<<<< HEAD
                 c = {
-=======
-                u = {
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-=======
-                u = {
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
                     13: 0
                 },
                 s = {
@@ -106,72 +65,45 @@
                     l: !1,
                     exports: {}
                 };
-                return f[e].call(t.exports, t, t.exports, d), t.l = !0, t.exports
+                return i[e].call(t.exports, t, t.exports, d), t.l = !0, t.exports
             }
             d.e = function(l) {
                 var e = [];
-                u[l] ? e.push(u[l]) : 0 !== u[l] && {
+                c[l] ? e.push(c[l]) : 0 !== c[l] && {
                     4: 1,
                     5: 1,
                     7: 1,
                     12: 1
-<<<<<<< HEAD
-<<<<<<< HEAD
                 }[l] && e.push(c[l] = new Promise(function(e, n) {
-=======
-                }[l] && e.push(u[l] = new Promise(function(e, n) {
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-=======
-                }[l] && e.push(u[l] = new Promise(function(e, n) {
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
                     for (var t = "static/css/" + ({}[l] || l) + "." + {
                             0: "31d6cfe0",
                             2: "31d6cfe0",
                             3: "31d6cfe0",
-                            4: "999c1a33",
-                            5: "1b824000",
+                            4: "689e62f3",
+                            5: "14f3b066",
                             6: "31d6cfe0",
-                            7: "8eacb140",
+                            7: "e7e12414",
                             8: "31d6cfe0",
                             9: "31d6cfe0",
                             10: "31d6cfe0",
-                            12: "7329fae5"
+                            12: "1e466373"
                         }[l] + ".chunk.css", o = d.p + t, r = document.getElementsByTagName("link"), a = 0; a < r.length; a++) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-                        var u = (f = r[a]).getAttribute("data-href") || f.getAttribute("href");
-                        if ("stylesheet" === f.rel && (u === t || u === o)) return e()
+                        var u = (i = r[a]).getAttribute("data-href") || i.getAttribute("href");
+                        if ("stylesheet" === i.rel && (u === t || u === o)) return e()
                     }
                     var c = document.getElementsByTagName("style");
                     for (a = 0; a < c.length; a++) {
-                        var f;
-                        if ((u = (f = c[a]).getAttribute("data-href")) === t || u === o) return e()
-=======
-                        var c = (i = r[a]).getAttribute("data-href") || i.getAttribute("href");
-                        if ("stylesheet" === i.rel && (c === t || c === o)) return e()
-                    }
-=======
-                        var c = (i = r[a]).getAttribute("data-href") || i.getAttribute("href");
-                        if ("stylesheet" === i.rel && (c === t || c === o)) return e()
-                    }
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-                    var u = document.getElementsByTagName("style");
-                    for (a = 0; a < u.length; a++) {
                         var i;
-                        if ((c = (i = u[a]).getAttribute("data-href")) === t || c === o) return e()
-<<<<<<< HEAD
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-=======
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
+                        if ((u = (i = c[a]).getAttribute("data-href")) === t || u === o) return e()
                     }
-                    var i = document.createElement("link");
-                    i.rel = "stylesheet", i.type = "text/css", i.onload = e, i.onerror = function(e) {
+                    var f = document.createElement("link");
+                    f.rel = "stylesheet", f.type = "text/css", f.onload = e, f.onerror = function(e) {
                         var t = e && e.target && e.target.src || o,
                             r = new Error("Loading CSS chunk " + l + " failed.\n(" + t + ")");
                         r.request = t, n(r)
-                    }, i.href = o, document.getElementsByTagName("head")[0].appendChild(i)
+                    }, f.href = o, document.getElementsByTagName("head")[0].appendChild(f)
                 }).then(function() {
-                    u[l] = 0
+                    c[l] = 0
                 }));
                 var r = s[l];
                 if (0 !== r)
@@ -184,34 +116,19 @@
                         var n, o = document.getElementsByTagName("head")[0],
                             a = document.createElement("script");
                         a.charset = "utf-8", a.timeout = 120, d.nc && a.setAttribute("nonce", d.nc), a.src = d.p + "static/js/" + ({}[l] || l) + "." + {
-                            0: "b728a061",
-<<<<<<< HEAD
-<<<<<<< HEAD
-                            2: "67dcffcf",
-                            3: "f7ed782f",
-                            4: "7e851895",
-                            5: "f560f1b3",
-                            6: "9abc14ba",
-                            7: "d18f683e",
-=======
-=======
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-                            2: "ed11143c",
-                            3: "16d0bf2c",
-                            4: "ab336fe7",
-                            5: "cfc61c31",
-                            6: "9abc14ba",
-                            7: "560dae1f",
-<<<<<<< HEAD
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-=======
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-                            8: "a75f5b19",
-                            9: "39e5f7f9",
-                            10: "6c10e498",
-                            12: "38899b9b"
+                            0: "8d72ab3f",
+                            2: "93bd7a24",
+                            3: "fb546b1c",
+                            4: "aba54bfd",
+                            5: "dc46d843",
+                            6: "639be01e",
+                            7: "6f4bec89",
+                            8: "fce9b566",
+                            9: "36bc6de8",
+                            10: "9cb0a604",
+                            12: "b969da75"
                         }[l] + ".chunk.js", n = function(e) {
-                            a.onerror = a.onload = null, clearTimeout(c);
+                            a.onerror = a.onload = null, clearTimeout(u);
                             var t = s[l];
                             if (0 !== t) {
                                 if (t) {
@@ -223,7 +140,7 @@
                                 s[l] = void 0
                             }
                         };
-                        var c = setTimeout(function() {
+                        var u = setTimeout(function() {
                             n({
                                 type: "timeout",
                                 target: a
@@ -232,7 +149,7 @@
                         a.onerror = a.onload = n, o.appendChild(a)
                     }
                 return Promise.all(e)
-            }, d.m = f, d.c = r, d.d = function(e, t, r) {
+            }, d.m = i, d.c = r, d.d = function(e, t, r) {
                 d.o(e, t) || Object.defineProperty(e, t, {
                     enumerable: !0,
                     get: r
@@ -272,69 +189,61 @@
             t.push = e, t = t.slice();
             for (var o = 0; o < t.length; o++) e(t[o]);
             var p = n;
-            i()
+            f()
         }([])
     </script>
-    <script src="{{ asset('static/js/11.e52230ec.chunk.js')}}"></script>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <script src="{{ asset('static/js/main.c6627c74.chunk.js')}}"></script>
-=======
-    <script src="{{ asset('static/js/main.30c9db4d.chunk.js')}}"></script>
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-=======
-    <script src="{{ asset('static/js/main.30c9db4d.chunk.js')}}"></script>
->>>>>>> 78f8fc13efb45b801b33503954138d90328b3b44
-     <script>
-     $(document).ready(function () {
+    <script src="{{ asset('static/js/11.dd2d47c4.chunk.js')}}"></script>
+    <script src="{{ asset('static/js/main.771994b8.chunk.js')}}"></script>
+    <script>
+        $(document).ready(function() {
+            $('.iconbar-container').click(function() {
+                $(this).toggleClass('change');
+                $(".navbar-sticky-bg").fadeIn('slow', function() {
+                    $("body").css({
+                        overflow: "hidden"
+                    });
+                    $('.navbar-sticky').animate({
+                        'left': $(window).outerWidth() - $(".navbar-sticky").outerWidth()
+                    }, 'fast')
+                })
+            });
+            $("body").click(function(e) {
+                if (e.target.classList.contains('navbar-sticky-bg') || e.target.classList.contains('list-group-item')) {
+                    $('.navbar-sticky').animate({
+                        "left": "100%"
+                    }, 'fast');
+                    $('.navbar-sticky-bg').fadeOut('slow', function() {
+                        $("body").css({
+                            overflow: "visible"
+                        });
+                        $('.iconbar-container').toggleClass('change')
+                    })
+                }
+            })
+            $(".list-group-item a").click(function(e) {
+                $('.navbar-sticky').animate({
+                    "left": "100%"
+                }, 'fast');
+                $('.navbar-sticky-bg').fadeOut('slow', function() {
+                    $("body").css({
+                        overflow: "visible"
+                    });
+                    $('.iconbar-container').toggleClass('change')
+                })
+            })
+        });
+    </script>
+    <!-- Global site tag(gtag.js)-Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131109496-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-         $('.iconbar-container').click(function () {
+        function gtag() {
+            dataLayer.push(arguments)
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-131109496-1');
+    </script>
+</body>
 
-             $(this).toggleClass('change');
-
-             $(".navbar-sticky-bg").fadeIn('slow',function () {
-                 $("body").css({overflow : "hidden"});
-                 $('.navbar-sticky').animate({'left' : $(window).outerWidth()-$(".navbar-sticky").outerWidth()},'fast');
-
-             });
-
-         });
-
-         $("body").click(function (e) {
-             // console.log(e.target.classList);
-             if(e.target.classList.contains('navbar-sticky-bg') || e.target.classList.contains('list-group-item')){
-                 // console.log("test navbar");
-                 $('.navbar-sticky')
-                     .animate({"left": "100%"}, 'fast');
-
-                 $('.navbar-sticky-bg').fadeOut('slow',function () {
-                     $("body").css({overflow : "visible"});
-                     $('.iconbar-container').toggleClass('change');
-                 });
-             }
-         })
-
-         $(".list-group-item a").click(function (e) {
-             $('.navbar-sticky')
-                 .animate({"left": "100%"}, 'fast');
-
-             $('.navbar-sticky-bg').fadeOut('slow',function () {
-                 $("body").css({overflow : "visible"});
-                 $('.iconbar-container').toggleClass('change');
-             });
-         })
-
-     });
-     </script>
-     <!-- Global site tag (gtag.js) - Google Analytics -->
-     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131109496-1"></script>
-     <script>
-       window.dataLayer = window.dataLayer || [];
-       function gtag(){dataLayer.push(arguments);}
-       gtag('js', new Date());
-
-       gtag('config', 'UA-131109496-1');
-     </script>
-
-    </body>
 </html>
